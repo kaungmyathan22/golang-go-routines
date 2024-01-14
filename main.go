65f8e382ea66a226f8a222ae5f154b63180d5338
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-01-14 14:08:15 +0630

    update(): Mutex example with thread safe operation

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "sync"
+import racecondition "github.com/kaungmyathan22/golang-go-routine/race_condition"
 
 // func printSomething(s string, wg *sync.WaitGroup) {
 // 	defer wg.Done()
@@ -29,30 +29,34 @@ import "sync"
 // 	printSomething("This is the second thing to be printed.", &wg)
 // }
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
+
+// func main() {
+
+// 	// challenge: modify this code so that the calls to updateMessage() on lines
+// 	// 28, 30, and 33 run as goroutines, and implement wait groups so that
+// 	// the program runs properly, and prints out three different messages.
+// 	// Then, write a test for all three functions in this program: updateMessage(),
+// 	// printMessage(), and main().
+
+// 	msg = "Hello, world!"
+
+// 	wg.Add(1)
+// 	go updateMessage("Hello, universe!", &wg)
+// 	wg.Wait()
+// 	printMessage()
+
+// 	wg.Add(1)
+// 	go updateMessage("Hello, cosmos!", &wg)
+// 	wg.Wait()
+// 	printMessage()
+
+//		wg.Add(1)
+//		go updateMessage("Hello, world!", &wg)
+//		wg.Wait()
+//		printMessage()
+//	}
 
 func main() {
-
-	// challenge: modify this code so that the calls to updateMessage() on lines
-	// 28, 30, and 33 run as goroutines, and implement wait groups so that
-	// the program runs properly, and prints out three different messages.
-	// Then, write a test for all three functions in this program: updateMessage(),
-	// printMessage(), and main().
-
-	msg = "Hello, world!"
-
-	wg.Add(1)
-	go updateMessage("Hello, universe!", &wg)
-	wg.Wait()
-	printMessage()
-
-	wg.Add(1)
-	go updateMessage("Hello, cosmos!", &wg)
-	wg.Wait()
-	printMessage()
-
-	wg.Add(1)
-	go updateMessage("Hello, world!", &wg)
-	wg.Wait()
-	printMessage()
+	racecondition.RaceConditionMain()
 }
